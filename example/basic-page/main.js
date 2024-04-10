f337a3d90af2fa826794c79946c29e02d1bb8c21
Nguyen Xuan Huong <nguyenhuongw@gmail.com>
2024-04-10 22:38:29 +0700

    Reduce upload accuracy&time (#5)

--- example/basic-page/main.js
+++ example/basic-page/main.js
@@ -21,6 +21,30 @@ const custom = {
   rpkiInvalidHost: null,
   cfTraceUrl: null,
   includeCredentials: false,
+  // Measurements configuration took from default with reduced upload
+  measurements: [
+    { type: 'latency', numPackets: 1 }, // initial ttfb estimation
+    { type: 'download', bytes: 1e5, count: 1, bypassMinDuration: true }, // initial download estimation
+    { type: 'latency', numPackets: 20 },
+    { type: 'download', bytes: 1e5, count: 9 },
+    { type: 'download', bytes: 1e6, count: 8 },
+    { type: 'upload', bytes: 1e3, count: 8 },
+    {
+      type: 'packetLoss',
+      numPackets: 1e3,
+      batchSize: 10,
+      batchWaitTime: 10, // ms (in between batches)
+      responsesWaitTime: 3000 // ms (silent time after last sent msg)
+    },
+    //{ type: 'upload', bytes: 1e6, count: 6 },
+    { type: 'download', bytes: 1e7, count: 6 },
+    //{ type: 'upload', bytes: 1e7, count: 4 },
+    { type: 'download', bytes: 2.5e7, count: 4 },
+    //{ type: 'upload', bytes: 2.5e7, count: 4 },
+    { type: 'download', bytes: 1e8, count: 3 },
+    // { type: 'upload', bytes: 5e7, count: 3 },
+    { type: 'download', bytes: 2.5e8, count: 2 }
+  ]
 };
 
 const engine = new SpeedTest(upDownUrl && turnUrl ? custom : { autoStart: true });
